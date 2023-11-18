3b4294b86e2229d51114cb7ce0381cc348683095
Ahalya-KT <ahalyakt002@gmail.com>
2023-11-18 14:30:19 +0530

    blogs

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Landingpage from "./pages/Landing/Landingpage";
+
+function App() {
+  return (
+    <div>
+      <Landingpage />
+    </div>
+  );
+}
+
+export default App;
